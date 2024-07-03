a923722415eb9a36fd866fec51abf51b62962dc9
declspecl <gavind2559@gmail.com>
2024-07-03 19:39:03 -0400

    newspaper text in good checkpoint state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { NewspaperText } from "./components/typography/NewspaperText";
 
 export function App() {
     const [count, setCount] = useState(0)
 
+    const MemoizedNewspaperText = memo(NewspaperText);
+
     return (
         <div>
-            <NewspaperText text="count"></NewspaperText>
+            <MemoizedNewspaperText text="Count" />
 
             <button onClick={() => setCount(count + 1)}>Increment</button>
         </div>
--- src/components/typography/NewspaperText.tsx
+++ src/components/typography/NewspaperText.tsx
@@ -1,25 +1,90 @@
-import React, { useMemo, useState } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import clsx from "clsx";
+import React from "react";
+import { cn } from "~/lib/utils";
+
+const NewspaperLetterVariants = cva(
+    "relative inline-block border-x-[0.125rem]",
+    {
+        variants: {
+            variant: {
+                whiteOnBlack: "bg-black border-black text-white",
+                blackOnWhite: "bg-white border-white text-black"
+            },
+            font: {
+                times: "font-times font-medium",
+                cooper: "font-cooper font-normal",
+                quilon: "font-quilon font-extralight"
+            },
+            size: {
+                "5xl": "text-5xl"
+            }
+        },
+        defaultVariants: {
+            variant: "whiteOnBlack",
+            size: "5xl"
+        }
+    }
+);
+
+interface NewspaperLetterProps
+extends
+    React.HTMLAttributes<HTMLSpanElement>,
+    VariantProps<typeof NewspaperLetterVariants>
+{}
+
+export function NewspaperLetter({ className, variant, font, ...props }: NewspaperLetterProps) {
+    return (
+        <span
+            className={clsx(
+                NewspaperLetterVariants({ variant, font }),
+                className
+            )}
+            {...props}
+        />
+    );
+
+}
 
 interface NewspaperTextProps extends React.HTMLAttributes<HTMLParagraphElement> {
     text: string
 }
 
 export function NewspaperText({ text, className, ...props }: NewspaperTextProps) {
-    const [displayText, setDisplayText] = useState("");
-
-    useMemo(() => {
-        let displayText = "";
-
-        for (const entry of text.split("").entries()) {
-            displayText += entry;
-        }
-
-        setDisplayText(displayText);
-    }, [text]);
+    const MIN_Y_OFFSET = -5, MAX_Y_OFFSET = 5;
 
     return (
-        <p className={className} {...props}>
-            {displayText}
+        <p className={cn("tracking-[-0.1rem]", className)} {...props}>
+            {text.split("").map(char => {
+                let yOffset = Math.random() * (MAX_Y_OFFSET - MIN_Y_OFFSET) + MIN_Y_OFFSET;
+
+                // make lowercase letters have less variation in y offset
+                if (char.toLocaleLowerCase() === char) {
+                    yOffset *= 0.5;
+                }
+
+                const isWhiteOnBlack = Math.random() < 0.95;
+                const fontChoiceNumber = Math.floor(Math.random() * 3);
+                
+                return (
+                    <NewspaperLetter
+                        variant={isWhiteOnBlack ? "whiteOnBlack" : "blackOnWhite"}
+                        font={
+                            fontChoiceNumber === 0
+                            ? "times"
+                            : fontChoiceNumber === 1
+                            ? "cooper"
+                            : "quilon"
+                        }
+                        className="relative"
+                        style={{
+                            top: `${yOffset}px`
+                        }}
+                    >
+                        {char}
+                    </NewspaperLetter>
+                )
+            })}
         </p>
     );
 }
