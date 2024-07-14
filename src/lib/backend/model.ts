fca6f5c9db6e1e5cb43109a7329237746ae16f20
declspecl <gavind2559@gmail.com>
2024-07-13 23:57:11 -0400

    create persona data

--- src/lib/backend/model.ts
+++ src/lib/backend/model.ts
@@ -1,30 +1,57 @@
 export enum Arcana {
-    "Fool",
-    "Magician",
-    "Priestess",
-    "Empress",
-    "Emperor",
-    "Hierophant",
-    "Lovers",
-    "Chariot",
-    "Justice",
-    "Hermit",
-    "Fortune",
-    "Strength",
-    "Hanged",
-    "Death",
-    "Temperance",
-    "Devil",
-    "Tower",
-    "Star",
-    "Moon",
-    "Sun",
-    "Judgement",
-    "World"
+    Fool,
+    Magician,
+    Priestess,
+    Empress,
+    Emperor,
+    Hierophant,
+    Lovers,
+    Chariot,
+    Justice,
+    Hermit,
+    Fortune,
+    Strength,
+    Hanged,
+    Death,
+    Temperance,
+    Devil,
+    Tower,
+    Star,
+    Moon,
+    Sun,
+    Judgement,
+    Faith,
+    Councillor
 }
 
-export interface Persona {
+export enum Stat {
+    Strength,
+    Magic,
+    Endurance,
+    Agility,
+    Luck
+}
+
+export enum DamageType {
+    Phys,
+    Gun,
+    Fire,
+    Ice,
+    Electric,
+    Wind,
+    Psychokinesis,
+    Nuclear,
+    Bless,
+    Curse,
+    Almighty
+}
+
+export interface PersonaData
+{
     name: string,
     level: number,
     arcana: Arcana,
+    highestStats: Stat[],
+    resistances: DamageType[],
+    weaknesses: DamageType[]
 }
