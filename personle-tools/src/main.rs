5403a3cb11971056e58803b78920f42946b7fce6
declspecl <gavind2559@gmail.com>
2024-10-13 20:19:46 -0400

    formatting

--- personle-tools/src/main.rs
+++ personle-tools/src/main.rs
@@ -31,19 +31,15 @@ async fn main() -> anyhow::Result<()> {
     fs::write("out/persona-names.txt", persona_names.join("\n")).await?;
 
     let mut persona_data_json = serde_json::to_string_pretty(&persona_data_by_name)?;
-
     for arcana in Arcana::iter() {
         persona_data_json = persona_data_json.replace(&format!("Arcana.{:?}", arcana), &format!("{:?}", arcana));
     }
-
     for stat in Stat::iter() {
         persona_data_json = persona_data_json.replace(&format!("Stat.{:?}", stat), &format!("{:?}", stat));
     }
-
     for damage_type in DamageType::iter() {
         persona_data_json = persona_data_json.replace(&format!("DamageType.{:?}", damage_type), &format!("{:?}", damage_type));
     }
-
     for fusion_method in model::FusionMethod::iter() {
         persona_data_json = persona_data_json.replace(&format!("FusionMethod.{:?}", fusion_method), &format!("{:?}", fusion_method));
     }
@@ -53,19 +49,15 @@ async fn main() -> anyhow::Result<()> {
     let persona_data_list: Vec<PersonaData> = persona_data_by_name.into_iter().map(|(_name, data)| data).collect();
 
     let mut persona_data_ts_object = serde_json::to_string_pretty(&persona_data_list)?;
-
     for arcana in Arcana::iter() {
         persona_data_ts_object = persona_data_ts_object.replace(&format!("\"Arcana.{:?}\"", arcana), &format!("Arcana.{:?}", arcana));
     }
-
     for stat in Stat::iter() {
         persona_data_ts_object = persona_data_ts_object.replace(&format!("\"Stat.{:?}\"", stat), &format!("Stat.{:?}", stat));
     }
-
     for damage_type in DamageType::iter() {
         persona_data_ts_object = persona_data_ts_object.replace(&format!("\"DamageType.{:?}\"", damage_type), &format!("DamageType.{:?}", damage_type));
     }
-
     for fusion_method in model::FusionMethod::iter() {
         persona_data_ts_object = persona_data_ts_object.replace(&format!("\"FusionMethod.{:?}\"", fusion_method), &format!("FusionMethod.{:?}", fusion_method));
     }
