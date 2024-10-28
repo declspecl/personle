2c4900214d54978f6edd1c010e6286964ba5b9cf
declspecl <gavind2559@gmail.com>
2024-10-27 21:19:44 -0400

    set cdk region to us-east-2

--- personle-cdk/bin/personle-cdk.ts
+++ personle-cdk/bin/personle-cdk.ts
@@ -3,4 +3,6 @@ import * as cdk from "aws-cdk-lib";
 import { PersonleCdkStack } from "../lib/personle-cdk-stack";
 
 const app = new cdk.App();
-new PersonleCdkStack(app, "PersonleCdkStack");
+new PersonleCdkStack(app, "PersonleCdkStack", {
+    env: { region: "us-east-2" }
+});
