56f6d47543d5ca11920bbbef2529bb6a1d4a349c
declspecl <gavind2559@gmail.com>
2024-10-27 21:46:49 -0400

    set ddb billing mode to on-demand, update removal policies

--- personle-cdk/lib/personle-cdk-stack.ts
+++ personle-cdk/lib/personle-cdk-stack.ts
@@ -1,4 +1,5 @@
 import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as ddb from "aws-cdk-lib/aws-dynamodb";
 import { Stack, StackProps } from "aws-cdk-lib";
@@ -16,13 +17,17 @@ export class PersonleCdkStack extends Stack {
 			sortKey: {
 				name: "sk",
 				type: ddb.AttributeType.STRING
-			}
+			},
+			billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+			removalPolicy: cdk.RemovalPolicy.DESTROY
 		});
 
 		const dailyPersonasBucket = new s3.Bucket(this, "DailyPersonas", {
 			bucketName: "daily-personas",
 			encryption: s3.BucketEncryption.S3_MANAGED,
-			accessControl: s3.BucketAccessControl.PRIVATE
+			accessControl: s3.BucketAccessControl.PRIVATE,
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			autoDeleteObjects: true
 		});
 	}
 }
