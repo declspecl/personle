f4bada2d217910a9920c4e61bf6513f001024ceb
declspecl <gavind2559@gmail.com>
2024-11-03 03:38:05 -0500

    update contour noise to webp

--- personle-website/src/components/graphics/ContourBackground.tsx
+++ personle-website/src/components/graphics/ContourBackground.tsx
@@ -3,5 +3,5 @@ import { cn } from "@lib/utils";
 interface ContourBackgroundProps extends React.HTMLAttributes<HTMLImageElement> {}
 
 export function ContourBackground({ className, ...props }: ContourBackgroundProps) {
-	return <div className={cn("bg-red bg-[url(/assets/images/contour-noise.png)] bg-contain", className)} {...props} />;
+	return <div className={cn("bg-red bg-[url(/assets/images/contour-noise.webp)] bg-contain", className)} {...props} />;
 }
