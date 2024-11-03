4cf13f8f96d69d591130fb0b84faa1768e7bac67
declspecl <gavind2559@gmail.com>
2024-11-03 04:07:30 -0500

    update to canonical www subdomain, minify built HTML

--- personle-website/vite.config.ts
+++ personle-website/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -37,5 +38,5 @@ export default defineConfig({
 			}
 		}
 	},
-	plugins: [react()]
+	plugins: [react(), ViteMinifyPlugin()]
 });
