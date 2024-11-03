c5885b141a739bb3af54381be491027ece1a730a
declspecl <gavind2559@gmail.com>
2024-11-03 03:47:24 -0500

    split up build and deploy scripts, create service for spring boot API

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+# clean build frontend
 cd personle-website
 rm -rf dist
 npm run build
-sudo mkdir -p /var/www/personle
-sudo mv dist/* /var/www/personle
-sudo chmod -R 755 /var/www/personle
 cd ..
 
+# clean build backend
 cd personle-api
 mvn clean package
-sudo mkdir -p /opt/personle/
-sudo mv target/personle-api.jar /opt/personle
 cd ..
-
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+./build.sh
+
+# move frontend files
+sudo mkdir -p /var/www/personle
+sudo mv dist/* /var/www/personle
+sudo chmod -R 755 /var/www/personle
+
+# move backend files
+sudo mkdir -p /opt/personle/
+sudo mv target/personle-api.jar /opt/personle
+
+# set up nginx
+sudo rm /etc/nginx/nginx.conf
+sudo cp conf/nginx.conf /etc/nginx/
+sudo systemctl enable nginx
+sudo systemctl restart nginx
+
+# set up personle service
+sudo rm /etc/systemd/system/personle.service
+sudo cp conf/personle.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/personle.service
+sudo chown root:root /etc/systemd/system/personle.service
+sudo useradd -r personle
+sudo chown -R personle:personle /opt/personle
+sudo systemctl daemon-reload
+sudo systemctl enable personle
+sudo systemctl start personle
