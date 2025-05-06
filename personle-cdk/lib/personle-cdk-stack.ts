de64fdece7f295d4179905a357b7ee68dde66765
declspecl <gavind2559@gmail.com>
2025-05-06 01:39:41 -0400

    deploy personle lambda

--- personle-cdk/lib/personle-cdk-stack.ts
+++ personle-cdk/lib/personle-cdk-stack.ts
@@ -3,6 +3,10 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as ddb from "aws-cdk-lib/aws-dynamodb";
 import { Stack, StackProps } from "aws-cdk-lib";
+import * as apigw from "@aws-cdk/aws-apigatewayv2-alpha";
+import * as integrations from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { execFileSync } from "child_process";
 
 export class PersonleCdkStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
@@ -28,6 +32,34 @@ export class PersonleCdkStack extends Stack {
 			accessControl: s3.BucketAccessControl.PRIVATE,
 			removalPolicy: cdk.RemovalPolicy.DESTROY,
 			autoDeleteObjects: true
+		});
+
+		execFileSync("npm", ["run", "package"], {
+			cwd: "../personle-lambda",
+			stdio: "inherit"
+		});
+		const personleLambda = new lambda.Function(this, "PersonleLambda", {
+            functionName: "PersonleLambda",
+			runtime: new lambda.Runtime("nodejs22.x"),
+			handler: "dist/main.handler",
+			code: lambda.Code.fromAsset("../personle-lambda/function.zip")
+		});
+		userGameDataTable.grantReadWriteData(personleLambda);
+		dailyPersonasBucket.grantReadWrite(personleLambda);
+
+		const httpApi = new apigw.HttpApi(this, "PersonleHttpApi", {
+            apiName: "PersonleHttpApi",
+			corsPreflight: {
+				allowOrigins: ["*"],
+				allowMethods: [apigw.CorsHttpMethod.GET, apigw.CorsHttpMethod.POST],
+				allowHeaders: ["*"]
+			}
+		});
+
+		httpApi.addRoutes({
+			path: "/api/daily-guesses",
+			methods: [apigw.HttpMethod.GET, apigw.HttpMethod.POST],
+			integration: new integrations.HttpLambdaIntegration("PersonleLambdaIntegration", personleLambda)
 		});
 	}
 }
